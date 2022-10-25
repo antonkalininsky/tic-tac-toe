ef75fff20d418d8d0e1572a0aac90b57bc40e987
antonkalininsky <chadik.free@gmail.com>
2022-10-25 19:30:48 +0600

    * Добавил логику хода и смену хода * Добавил работу строки сообщений * Добавил проверку условий выйгрыша или ничьей

--- app.js
+++ app.js
@@ -3,42 +3,133 @@ let grid = [
     [0, 0, 0],
     [0, 0, 0]
 ];
-
 let o = "oval";
 let x = "cross";
-
 let size = 3;
+let turn = 2;
+let endGame = false;
+let stepCou = 0;
 
-let turn = 1;
-
-$(document).ready(function(){
+$(document).ready(function () {
+    // initialization
+    initGame();
     updateMarks();
+    updateMsg();
 
-    $(".cell").click(function () { 
+    // click tile
+    $(".cell").click(function () {
         let str = this.className.split(" ")[1].split("-");
         let y = parseFloat(str[1]) - 1;
         let x = parseFloat(str[2]) - 1;
-
-        grid[y][x] = 2;
-
+        makeTurn(y, x);
         updateMarks();
+        updateMsg();
+    });
+
+    // reset game
+    $(".reset").click(function () {
+        initGame();
+        updateMarks();
+        updateMsg();
     });
 
 
-
-    $(".reset").click(function () { 
-        grid = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ];
-        
-        updateMarks();
-    });
-
-    
 });
 
+
+
+function updateMsg() {
+    let target = $(".info");
+    target.empty();
+    if (endGame) {
+        target.append("Game over! ");
+        if (stepCou === 9) {
+            target.append("Draw!");
+        } else {
+            target.append("Player ");
+            if (turn === 1) {
+                target.append("1");
+            } else {
+                target.append("2");
+            }
+            target.append(" won!");    
+        }
+
+        
+    } else {
+        target.append("Player's ");
+        if (turn === 1) {
+            target.append("2");
+        } else {
+            target.append("1");
+        }
+        target.append(" turn...");
+    }
+}
+
+function checkWin() {
+    let i = 0;
+    let j = 0;
+    while (i != size) {
+        if (grid[i][j] != 0 && grid[i][j] === grid[i][j + 1] && grid[i][j + 1] === grid[i][j + 2]) {
+            endGame = true;
+            return;
+        }
+        i++;
+    }
+
+    i = 0;
+    j = 0;
+    while (j != size) {
+        if (grid[i][j] != 0 && grid[i][j] === grid[i + 1][j] && grid[i + 1][j] === grid[i + 2][j]) {
+            endGame = true;
+            return;
+        }
+        j++;
+    }
+
+    if (grid[1][1] != 0 &&
+        (grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2] ||
+        grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0])) {
+        endGame = true;
+        return;
+    }
+
+    if (stepCou === 9) {
+        endGame = true;
+        draw = true;
+    }
+
+}
+
+function makeTurn(Y, X) {
+    if (grid[Y][X] != 0 || endGame) {
+        return;
+    }
+
+    stepCou++;
+    if (turn === 1) {
+        turn = 2;
+        grid[Y][X] = 2;
+    } else {
+        turn = 1;
+        grid[Y][X] = 1;
+    }
+    checkWin();
+}
+
+
+
+function initGame() {
+    endGame = false;
+    turn = 2;
+    grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+    stepCou = 0;
+}
 
 
 
@@ -47,10 +138,8 @@ function updateMarks() {
     while (i != size) {
         let j = 0;
         while (j != size) {
-            if (grid[i][j] === 0) {
-                $(toString(i, j) + " > .mark").removeClass(o);
-                $(toString(i, j) + " > .mark").removeClass(x);
-            }
+            $(toString(i, j) + " > .mark").removeClass(o);
+            $(toString(i, j) + " > .mark").removeClass(x);
             if (grid[i][j] === 1) {
                 $(toString(i, j) + " > .mark").addClass(x);
             }
